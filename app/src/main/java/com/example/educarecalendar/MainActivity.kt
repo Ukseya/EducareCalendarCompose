72434be2035551ad9a035b7ec2a37cbfb2ba3991
Ukseya <ukbeyahsi01@gmail.com>
2024-03-19 14:39:27 +0100

    Calendar Parent builds after eventArray is changed

--- app/src/main/java/com/example/educarecalendar/MainActivity.kt
+++ app/src/main/java/com/example/educarecalendar/MainActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import java.time.LocalDate
 import java.util.Locale
@@ -15,26 +17,34 @@ class MainActivity : ComponentActivity() {
 
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val handler = Handler()
-        requestEvents(this)
-        handler.postDelayed({
-            val eventArray = eventArrayEvents
-            Log.d("eventArraySizeInMain","${eventArray.size}")
+
+//        handler.postDelayed({
+            //val eventArray = eventArrayEvents
+
 
 
 
         setContent {
-
-            CalendarParent(
-                monthName = calendarSetter("month"),
-                year = calendarSetter("year"),
-                eventArray = eventArray
+            var wait = remember { mutableStateOf(false) }
+            var eventArray = remember { mutableStateOf(eventArrayEvents) }
+            requestEvents(this)
+            eventArray.value = eventArrayEvents
+            Log.d("eventArraySizeInMain","${eventArray.value.size}")
+            wait.value = eventArray.value.size!=0
+            if (wait.value) {
+                CalendarParent(
+                    monthName = calendarSetter("month"),
+                    year = calendarSetter("year"),
+                    eventArray = eventArray.value
                 )
             }
-        },5000)
+        }
+//        },5000)
 
     }
 
